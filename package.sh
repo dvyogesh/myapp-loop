901907941c40e7ae3b754d9f79391e8af3a0bb61
Yogesh <yogesh@Yogeshs-MacBook-Air.local>
2020-05-12 13:08:11 +0530

    first less logic

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-sleep_time=90
+sleep_time=20
 echo $1;
 
 number_of_loop=$1;
@@ -38,18 +38,18 @@ for((i=1;i<=number_of_loop;i++));
         START=$(date +%s) ;
         source_dir=$(find . -name \*loop\* -type d -maxdepth 1 -print -quit) ;
         echo "$source_dir" ;
-        random_string=`openssl rand -hex 12`;
+       # random_string=`openssl rand -hex 12`;
         dir_name_slsh="$source_dir/" ;
         echo "$dir_name_slsh" ;
-        mv $dir_name_slsh ./myapp-loop-$random_string;
+       # mv $dir_name_slsh ./myapp-loop-$random_string;
         sleep 5 ;
-        rm -rf $dir_name_slsh;
-        cd myapp-loop-$random_string;
+       # rm -rf $dir_name_slsh;
+        cd $source_dir;
         sleep 2;
         npm cache clean --force;
         # if not working count remove nodemodules folder
-        #rm -rf node_modules;
-        #rm -rf package-lock.json;
+        rm -rf node_modules;
+        rm -rf package-lock.json;
         sleep 5;
         npm i @yoge-shource/grid-carousel-react ;
         cd ..;
