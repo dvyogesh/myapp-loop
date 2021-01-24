10096a53cce33c6d37c4cefa3457adcd259672b2
Yogesh <yogesh@Yogeshs-MacBook-Air.local>
2021-01-24 22:10:23 +0530

    daily npm

---
+++ everydayNpmInstall.sh
@@ -0,0 +1,6 @@
+npm cache clean --force;
+        # if not working count remove nodemodules folder
+rm -rf node_modules;
+rm -rf package-lock.json;
+
+for((i=1;i<=2300;i++)); do echo $i;npm cache clean --force;npm i handle-window-undefined;npm i @yoge-shource/grid-carousel-react;npm i react-window-dimensions-hook;npm i parse-url-query-params;npm i disable-snapshot-testing;npm i @yoge-shource/grid-carousel-react;done;
